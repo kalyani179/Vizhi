7d0fe3bd34dabb6cb23343fb591269c15ac3bb55
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2023-12-10 16:49:17 +0530

    Ccompleted Image Section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,10 @@ import OurKeyFeature from './OurKeyFeature'
 function Home() {
   return (
     <div className='bg-gradient-to-b from-slate-300 to-slate-300'>
+      <div>
+        <img className='opacity-50' src="../../../images/home.png" alt="" />
+        <div class="absolute w-[600px] top-80 left-96 text-center text-zinc-800 text-3xl font-medium font-['Oldenburg']">"Vizhi: Uniting Communities for a Greener, Cleaner Future."</div>
+      </div>
       <OurKeyFeature />
     </div>
   )
